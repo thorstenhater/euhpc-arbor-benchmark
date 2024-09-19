0adb4ae9ff03a2158c1284634e9163665997506a
Thorsten Hater <24411438+thorstenhater@users.noreply.github.com>
2024-09-19 10:07:07 +0200

    Tweak input/batch files

--- gen-inputs.py
+++ gen-inputs.py
@@ -1,20 +1,13 @@
 #!/usr/bin/env python3
 
 # TODO: Change me; JUWELS values
-CPUS_PER_NODE = 96
+CPUS_PER_NODE = 32
 GPUS_PER_NODE = 4
-# TODO Change me, [first, last] node counts in log2 scale
-nodes_min = 1
-nodes_max = 8
-# TODO Change me, [first, last] cell counts (per GPU) in linear scale with step
-cells_min = 8000
-cells_inc = 8000
-cells_max = 48000
 
 def write_input(nodes, cells, out):
     raw = rf"""{{
-    "name": "run_nodes={nodes}_cells={cells}",
-    "num-cells": {cells},
+    "name": "run_nodes-{nodes}_cells-{cells}",
+    "num-cells": {cells*nodes*GPUS_PER_NODE},
     "synapses": 10,
     "min-delay": 5,
     "duration": 200,
@@ -41,29 +34,30 @@ def write_input(nodes, cells, out):
 
 def write_sbatch(nodes, cells, inp, out):
     raw = rf"""#!/bin/bash -x
-#SBATCH --job-name="busyring-cells={cells}-nodes={nodes}"
+#SBATCH --job-name="busyring-cells-{cells}-nodes-{nodes}"
 #SBATCH --mail-user=
 #SBATCH --mail-type=NONE
 #SBATCH --nodes={nodes}
-#SBATCH --ntasks={GPUS_PER_NODE}
+#SBATCH --ntasks={GPUS_PER_NODE*nodes}
 #SBATCH --cpus-per-task={CPUS_PER_NODE//GPUS_PER_NODE}
 #SBATCH --time=90
-#SBATCH --output=job-cells={cells}-nodes={nodes}.out
-#SBATCH --error=job-cells={cells}-nodes={nodes}.err
+#SBATCH --output=job-cells-{cells}-nodes-{nodes}.out
+#SBATCH --error=job-cells-{cells}-nodes-{nodes}.err
 #SBATCH --gres=gpu:{GPUS_PER_NODE}
+#SBATCH --account=try24_Arbor
+#SBATCH --partition=boost_usr_prod
 #SBATCH --exclusive
+
+ml cuda gcc openmpi
 
 srun busyring {inp}
     """
     with open(out, 'w') as fd:
         print(raw, file=fd)
 
-nodes = nodes_min
-while nodes <= nodes_max:
-    for cells in range(cells_min, cells_max + 1, cells_inc):
-        cells = cells * nodes * GPUS_PER_NODE
-        json = f"input-cells={cells}-nodes={nodes}.json"
-        job  = f"submit-cells={cells}-nodes={nodes}.job"
+for nodes in [1, 2, 4, 8, 16, 32, 64]:
+    for cells in [64000]:
+        json = f"input-cells-{cells}-nodes-{nodes}.json"
+        job  = f"submit-cells-{cells}-nodes-{nodes}.job"
         write_input(nodes, cells, out=json)
         write_sbatch(nodes, cells, inp=json, out=job)
-    nodes *= 2
